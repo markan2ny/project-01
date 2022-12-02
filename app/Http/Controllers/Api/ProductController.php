5db5d56cc4859e2f94a015275c309b60a34dbfcd
unknown <git.markan2nypascual@gmail.com>
2022-12-02 13:34:00 +0800

    Add Product Controller & Model

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return response()->json('HELLO WORLD');
+        $product = DB::table('products')->select('product_name')->get();
+        return response()->json($product);
+    }
+
+    public function store(ProductRequest $request)
+    {
+        Product::create($request->validated());
+
+        return response()->json($request);
+    }
+
+    public function update($id)
+    {
+        $product = DB::table('products')->where('id', $id)->first();
+        return response()->json($product);
     }
 }
