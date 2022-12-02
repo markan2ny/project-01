ef9186f3afd9ae3849f9693ae1ab5049e7458939
unknown <git.markan2nypascual@gmail.com>
2022-12-02 13:26:41 +0800

    Add Product Seeder

--- database/migrations/2022_12_02_011958_create_products_table.php
+++ database/migrations/2022_12_02_011958_create_products_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product_name');
+            $table->integer('product_qty');
             $table->timestamps();
         });
     }
