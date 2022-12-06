188ed0ec0841ef998765a0b300ac55df00a3ca14
unknown <git.markan2nypascual@gmail.com>
2022-12-06 10:56:48 +0800

    Add Dashboard Component

--- resources/js/pages/LoginPage.vue
+++ resources/js/pages/LoginPage.vue
@@ -1,67 +1,45 @@
 <template>
-
     <div class="block xl:w-[25vw] mt-[50px] mx-auto px-4 py-5 shadow-lg">
         {{ form }}
         <p v-if="error">{{ error }}</p>
         <form @submit.prevent="loginHandler">
             <h1 class="text-3xl font-squada bg-gradient-to-tr from-cyan-500 to-pink-500 text-transparent bg-clip-text">
-                Login</h1>
-            <input type="text" placeholder="Email Address"
+                Login
+            </h1>
+            <input type="email" placeholder="Email Address"
                 class="w-full p-2 bg-transparent text-slate-600 border-2 outline-none border-slate-500 mt-[20px]"
-                v-model="form.email">
+                v-model="form.email" />
             <input type="password" placeholder="Password"
-                class="w-full p-2 bg-transparent  text-slate-600 border-2 outline-none border-slate-500 mt-[20px] mb-[20px]"
-                v-model="form.password">
+                class="w-full p-2 bg-transparent text-slate-600 border-2 outline-none border-slate-500 mt-[20px] mb-[20px]"
+                v-model="form.password" />
             <button
-                class="py-2 px-5 font-bold text-slate-500 hover:text-pink-500 hover:border-pink-500 border-2 uppercase border-slate-500">Sign
-                in</button>
+                class="py-2 px-5 font-bold text-slate-500 hover:text-pink-500 hover:border-pink-500 border-2 uppercase border-slate-500">
+                Sign in
+            </button>
         </form>
     </div>
-
 </template>
 
-<script>
-import { reactive, ref } from 'vue';
-import axios from 'axios';
-
-export default {
-    setup() {
-        let form = reactive({
-            email: '',
-            password: ''
-        })
-        let error = ref('');
-        const loginHandler = async () => {
-
-            await axios.post('/api/login', form)
-                .then(res => {
-                    if (res.data.success)
-                        alert('success');
-                    if (!res.data.success)
-                        return error.value = res.data.message;
-                })
-                .catch(err => console.log(err))
-        }
-
-        return {
-            form,
-            loginHandler
-        }
-    }
-}
-
+<script setup>
+import { reactive, ref } from "vue";
+import axios from "axios";
 
 // composition api
-// let form = reactive({
-//     email: '',
-//     password: '',
-// })
-
-
-// const loginHandler = async () => {
-
-//     await axios.post('/api/login', form)
-//         .then(res => console.log(res))
-//         .catch(err => console.log(err))
-// }
+let form = reactive({
+    email: "",
+    password: "",
+});
+let error = ref('');
+const loginHandler = async () => {
+    await axios
+        .post("/api/login", form)
+        .then((res) => {
+            const { data } = res.data;
+            // Stored token to localstorage
+            localStorage.setItem("token", data[0]);
+        })
+        .catch((err) => {
+            error.value = err.response.data.message;
+        });
+};
 </script>
