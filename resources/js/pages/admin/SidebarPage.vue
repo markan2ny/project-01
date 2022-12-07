e67ee63178050527175cdfbf43258c3dc6c0f1c2
unknown <git.markan2nypascual@gmail.com>
2022-12-07 14:52:50 +0800

    Chnage sidebar text color

--- resources/js/pages/admin/SidebarPage.vue
+++ resources/js/pages/admin/SidebarPage.vue
@@ -3,7 +3,7 @@
         class="w-[150px] h-[80vh] shadow-md relative bg-gradient-to-br from-cyan-400 to-pink-400 rounded-md overflow-hidden">
         <div class="flex flex-col items-center py-10 h-full">
             <router-link :to="{ name: 'dashboard' }" class="font-squada text-4xl text-white">LOGO</router-link>
-            <ul class="grow mt-10">
+            <ul class="grow mt-10 text-white">
                 <li>
                     <router-link to="#">Employee</router-link>
                 </li>
